559328f161826680ab342e58c7de0e6c943b5fe0
matkuhlm <68480067+matkuhlm@users.noreply.github.com>
2020-12-01 20:47:44 -0500

    Update models.py
    Added tables

--- templates/models.py
+++ templates/models.py
@@ -1,24 +1,33 @@
-# Pulled in tables and columns with sqlacodegen --outfile
 # coding: utf-8
-from sqlalchemy import Column, ForeignKey, Integer, String, Table
+#pulled SQL tables using sqlacodegen
+from sqlalchemy import BigInteger, Column, Float, ForeignKey, Integer, String, Table, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 metadata = Base.metadata
 
 
+t_country_average_temperature = Table(
+    'country_average_temperature', metadata,
+    Column('Country', Text, index=True),
+    Column('Year', BigInteger),
+    Column('NOC', Text),
+    Column('AverageTemperature', Float(53))
+)
+
+
 class NocRegion(Base):
     __tablename__ = 'noc_regions'
 
-    region = Column(String(100), primary_key=True)
-    NOC = Column(String(10), nullable=False)
+    region = Column(String(100), nullable=False)
+    NOC = Column(String(10), primary_key=True)
 
 
 t_athlete_medals = Table(
     'athlete_medals', metadata,
     Column('Sex', String(2), nullable=False),
-    Column('Team', ForeignKey('noc_regions.region'), nullable=False),
-    Column('NOC', String(10), nullable=False),
+    Column('Team', String(100), nullable=False),
+    Column('NOC', ForeignKey('noc_regions.NOC'), nullable=False),
     Column('Games', String(100), nullable=False),
     Column('Year', Integer, nullable=False),
     Column('Season', String(50), nullable=False),
@@ -30,7 +39,8 @@ t_athlete_medals = Table(
 
 t_city_average_temperature = Table(
     'city_average_temperature', metadata,
-    Column('Country', ForeignKey('noc_regions.region'), nullable=False),
+    Column('region', String(100), nullable=False),
+    Column('NOC', ForeignKey('noc_regions.NOC'), nullable=False),
     Column('City', String(100), nullable=False),
     Column('Year', Integer, nullable=False),
     Column('Latitude', Integer, nullable=False),
@@ -41,16 +51,17 @@ t_city_average_temperature = Table(
 
 t_country_average_temperatue = Table(
     'country_average_temperatue', metadata,
-    Column('Country', ForeignKey('noc_regions.region'), nullable=False),
+    Column('Country', String(100), nullable=False),
     Column('Year', Integer, nullable=False),
+    Column('NOC', ForeignKey('noc_regions.NOC'), nullable=False),
     Column('AverageTemperature', Integer, nullable=False)
 )
 
 
 t_country_medals = Table(
     'country_medals', metadata,
-    Column('NOC', String(10), nullable=False),
-    Column('region', ForeignKey('noc_regions.region'), nullable=False),
+    Column('NOC', ForeignKey('noc_regions.NOC'), nullable=False),
+    Column('region', String(100), nullable=False),
     Column('Bronze', Integer, nullable=False),
     Column('Silver', Integer, nullable=False),
     Column('Gold', Integer, nullable=False)
