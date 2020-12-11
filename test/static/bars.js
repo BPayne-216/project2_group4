cb5895c27f891ca9cbe0fd582c8eb80f94929e73
paulhan49 <you@example.com>
2020-12-11 18:15:52 -0500

    update barchart

--- test/static/bars.js
+++ test/static/bars.js
@@ -1,102 +1,131 @@
-// Step 1: Set up our chart
-//= ================================
+// set the dimensions and margins of the graph
+d3.csv("../csv_data/total_medals_data.csv").then(d => chart(d))
+function chart(csv) {
+    var keys = csv.columns.slice(2);
+    var olympic = [...new Set(csv.map(d => d.Olympic))]
+    var noc = [...new Set(csv.map(d => d.NOC))]
+    var options = d3.select("#olympic").selectAll("option")
+        .data(olympic)
+        .enter().append("option")
+        .text(d => d)
+    var svg = d3.select("#chart"),
+        margin = { top: 20, left: 50, bottom: 30, right: 10 },
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom;
 
-var margin = {top: 20, right: 40, bottom: 60, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    var y = d3.scaleBand()
+        .range([margin.top, height - margin.bottom])
+        .padding(0.1)
+        .paddingOuter(0.2)
+        .paddingInner(0.2)
+
+    var x = d3.scaleLinear()
+        .range([margin.left, width - margin.right])
+
+    var yAxis = svg.append("g")
+        .attr("transform", `translate(${margin.left},0)`)
+        .attr("class", "y-axis")
 
 
-// parse the date / time
-var parseTime = d3.timeParse("%Y");
 
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+    var xAxis = svg.append("g")
+        .attr("transform", `translate(0,${margin.top})`)
+        .attr("class", "x-axis")
 
-// define the 1st line
-var valueline = d3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.USA); });
+    var z = d3.scaleOrdinal()
+        .range(["#FF9900", "#C0C0C0", "#FFD700"])
+        .domain(keys);
+    //////////////////////////
+    var tooltip = d3.select("chart")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "1px")
+        .style("padding", "1px")
+    /////////////////////////////////
+    var mouseover = function (d) {
+        var subgroupName = d3.select(this.parentNode).datum().key;
+        var subgroupValue = d.data[subgroupName];
+        tooltip
+            .html("Medal Type: " + subgroupName + "<br>" + "Total Medals: " + subgroupValue)
+            .style("opacity", 1)
+    }
+    ///////////////////////////////////////////
+    var mousemove = function (d) {
+        tooltip
+            .style("left", (d3.mouse(this)[1] + 120) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+            .style("top", (d3.mouse(this)[2]) + "px")
+    }
+    var mouseleave = function (d) {
+        tooltip
+            .style("opacity", 0)
+    }
+    update(d3.select("#olympic").property("value"), 0)
+    function update(input, speed) {
+        var data = csv.filter(f => f.Olympic == input)
+        data.forEach(function (d) {
+            d.total = d3.sum(keys, k => +d[k])
+            return d
+        })
+        x.domain([0, d3.max(data, d => d.total)]).nice();
+        svg.selectAll(".x-axis").transition().duration(speed)
+            .call(d3.axisTop(x).ticks(null, "s"))
+        data.sort(d3.select("#sort").property("checked")
+            ? (a, b) => b.total - a.total
+            : (a, b) => noc.indexOf(a.NOC) - noc.indexOf(b.State))
+        y.domain(data.map(d => d.NOC));
 
-// define the 2nd line
-var valueline2 = d3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.China); });
+        svg.selectAll(".y-axis").transition().duration(speed)
+            .call(d3.axisLeft(y).tickSizeOuter(0))
+        var group = svg.selectAll("g.layer")
+            .data(d3.stack().keys(keys)(data), d => d.key)
+        group.exit().remove()
+        group.enter().insert("g", ".y-axis").append("g")
+            .classed("layer", true)
+            .attr("fill", d => z(d.key));
+        var bars = svg.selectAll("g.layer").selectAll("rect")
+            .data(d => d, e => e.data.NOC);
+        bars.exit().remove()
 
-// define the 3rd line
-var valueline3 = d3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.Russia); });
+        bars.enter().append("rect")
+            .attr("height", y.bandwidth())
+            .merge(bars)
+            .transition().duration(speed)
+            .attr("y", d => y(d.data.NOC))
+            .attr("x", d => x(d[0]))
+            .attr("width", d => x(d[1]) - x(d[0]))
+        var text = svg.selectAll(".text")
+            .data(data, d => d.NOC);
+        text.exit().remove()
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+        text.enter().append("text")
+            .attr("class", "text")
+            .attr("text-anchor", "start")
+            .merge(text)
+            .transition().duration(speed)
+            .attr("y", d => y(d.NOC) + y.bandwidth() / 2)
+            .attr("x", d => x(d.total) + 5)
+            .text(d => d.total)
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave)
+        //
+        /////////////////
+        
+    }
 
-// Get the data
-d3.csv("dummy1.csv").then(function(data) {
 
-  // format the data
-  data.forEach(function(d) {
-      d.year = parseTime(d.year);
-      d.USA = +d.USA;
-      d.China = +d.China;
-      d.Russia = +d.Russia;
-  });
 
-  // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.year; }));
-  y.domain([0, d3.max(data, function(d) {
-	  return Math.max(d.USA, d.China, d.Russia); })]);
+    var select = d3.select("#olympic")
+        .on("change", function () {
+            update(this.value, 750)
+        })
+    var checkbox = d3.select("#sort")
+        .on("click", function () {
+            update(select.property("value"), 750)
+        })
 
-  // Add the valueline path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", valueline);
-
-  // Add the valueline2 path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .style("stroke", "red")
-      .attr("d", valueline2);
-
-  // Add the valueline2 path.
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .style("stroke", "blue")
-      .attr("d", valueline3);
-
-  // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-  
-  // text label for the x axis
-  svg.append("text")             
-      .attr("transform",
-            "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
-      .style("text-anchor", "middle")
-      .text("Year");
-
-  // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
-
-  // text label for the y axis
-  svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Country");    
-
-});
+}
