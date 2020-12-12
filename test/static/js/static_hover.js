3135b734d3012175d9fdf302d89fe0ff290c52e0
matkuhlm <68480067+matkuhlm@users.noreply.github.com>
2020-12-11 19:55:18 -0500

    hover update

--- test/static/js/static_hover.js
+++ test/static/js/static_hover.js
@@ -1,7 +1,7 @@
 // Creating map object
 var myMap = L.map("map", {
     center: [34.0522, -118.2437],
-    zoom: 8
+    zoom: 5
   });
   
   // Adding tile layer
@@ -26,7 +26,7 @@ var myMap = L.map("map", {
     geojson = L.choropleth(data, {
   
       // Define what  property in the features to use
-      valueProperty: "MHI2016",
+      valueProperty: "",
   
       // Set color scale
       scale: ["#ffffb2", "#b10026"],
@@ -45,27 +45,12 @@ var myMap = L.map("map", {
   
       // Binding a pop-up to each layer
       onEachFeature: function(feature, layer) {
-        layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
+        layer.bindPopup(": " + feature.properties.ZIP + "<br>:<br>" +
           "$" + feature.properties.MHI2016);
       }
     }).addTo(myMap);
   
-    // Set up the legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-      var limits = geojson.options.limits;
-      var colors = geojson.options.colors;
-      var labels = [];
-  
-      // Add min & max
-      var legendInfo = "<h1>Median Income</h1>" +
-        "<div class=\"labels\">" +
-          "<div class=\"min\">" + limits[0] + "</div>" +
-          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-        "</div>";
-  
-      div.innerHTML = legendInfo;
+    
   
       limits.forEach(function(limit, index) {
         labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
@@ -73,10 +58,10 @@ var myMap = L.map("map", {
   
       div.innerHTML += "<ul>" + labels.join("") + "</ul>";
       return div;
-    };
+    });
   
     // Adding legend to the map
     legend.addTo(myMap);
   
-  });
+  
   
